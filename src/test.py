669ec9d944c1c7d986101536a266b8bc02a01411
smr-j <123605258+smr-j@users.noreply.github.com>
2024-07-17 21:33:48 -0400

    Update test.py
    Shifting to pytest

--- src/test.py
+++ src/test.py
@@ -4,46 +4,61 @@ Last edited: 07/13/2024
 Associated with: CS6620: Cloud Computing - REST API Assignment
 Objective: This file contains tests for use with the REST API Assignment.
 '''
-import unittest
-import requests
+import pytest
 import os
+import pytest
+from rest import create_app
+
+@pytest.fixture()
+def app():
+	app = create_app()
+	app.config.update({
+	"TESTING": True,
+	})
+
+    # other setup can go here
+
+	yield app
+
+    # clean up / reset resources here
 
 
-class RESTTest(unittest.TestCase):
-	def test_get_songs(self):
-		url = os.environ.get('URL') + '/songs'
-		response = requests.get(url)
-		self.assertEqual(response.status_code, 200)
+@pytest.fixture()
+def client(app):
+	return app.test_client()
+
+
+def test_get_songs(client):
+	url = os.environ.get('URL') + '/songs'
+	response = client.get(url)
+	assert response.status_code == 200
           
-	def test_add_song(self):
-		new_song = {"id": 4,
-        "title":"Bethlehem Steel",
-        "artist":"Delta Rae",
-        "album":"After It All",
-        "release":"04-07-2017",
-        "spotify link":"https://open.spotify.com/track/0mwXgKxOrmSk2veQkLbWSJ?si=3b0d11732d134fda"
-        }
-		url = os.environ.get('URL') + '/songs'
-		response = requests.post(url,json=new_song)
-		self.assertEqual(response.status_code, 201)
+def test_add_song(client):
+	new_song = {"id": 4,
+		    "title":"Bethlehem Steel",
+		    "artist":"Delta Rae",
+		    "album":"After It All",
+		    "release":"04-07-2017",
+		    "spotify link":"https://open.spotify.com/track/0mwXgKxOrmSk2veQkLbWSJ?si=3b0d11732d134fda"
+		   }
+	url = os.environ.get('URL') + '/songs'
+	response = client.post(url,json=new_song)
+	assert response.status_code == 201
 
-	def test_update_song(self):
-		updated_song = {
+def test_update_song(client):
+	updated_song = {
 		"id": 2,
-        "title":"Storm Song",
-        "artist":"PHILDEL",
-        "album":"The Disappearance of the Girl",
-        "release":"02-04-2014",
-        "spotify link":"https://open.spotify.com/track/5GTziJvDhtcCR6kCr6Ir8r?si=c3ef1d4ee015422a"
-		}
-		url = os.environ.get('URL') + '/songs/2'
-		response = requests.put(url, json=updated_song)
-		self.assertEqual(response.status_code, 200)
-    
-	def test_delete_song(self):
-		url = os.environ.get('URL') + '/songs/2'
-		response = requests.delete(url)
-		self.assertEqual(response.status_code, 200)
+        	"title":"Storm Song",
+		"artist":"PHILDEL",
+		"album":"The Disappearance of the Girl",
+		"release":"02-04-2014",
+		"spotify link":"https://open.spotify.com/track/5GTziJvDhtcCR6kCr6Ir8r?si=c3ef1d4ee015422a"
+	}
+	url = os.environ.get('URL') + '/songs/2'
+	response = client.put(url, json=updated_song)
+	assert response.status_code == 200
 
-if __name__ == '__main__':
-    unittest.main()
+def test_delete_song(client):
+	url = os.environ.get('URL') + '/songs/2'
+	response = client.delete(url)
+	assert response.status_code == 200
